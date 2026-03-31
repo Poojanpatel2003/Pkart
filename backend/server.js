4aa3ca7ba7b0eed6a3cdc5cfe4bac5ae25c68090
Poojanpatel2003 <poojanpatel1595@gmail.com>
2026-03-31 10:52:04 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express=require('express')
+import express from 'express';
+import 'dotenv/config'
+import connectDB from './database/db.js'
 const app=express()
-const PORT=3000
+const PORT=process.env.PORT || 3000
 app.listen(PORT,()=>{
+    connectDB()
     console.log(`Server is running at port:${PORT}`)
 })
